cb13f2258e6cd4d1cd5929737ac2cfc28d7a0794
Andrew <andrewcoorey13@gmail.com>
2026-04-23 11:36:29 +1000

    robot urdf/sdf with both robot arms

--- src/mechelangelo_gazebo/launch/environment_arm.launch.py
+++ src/mechelangelo_gazebo/launch/environment_arm.launch.py
@@ -98,6 +98,9 @@ def generate_launch_description():
     y_pose = LaunchConfiguration('y_pose')
     z_pose = LaunchConfiguration('z_pose')
     yaw = LaunchConfiguration('yaw')
+    roll = LaunchConfiguration('roll')
+    pitch = LaunchConfiguration('pitch')
+    yaw = LaunchConfiguration('yaw')
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     existing_model_path = os.environ.get('GAZEBO_MODEL_PATH', '')
@@ -121,15 +124,15 @@ def generate_launch_description():
         )
     )
 
-    robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': robot_description,
-            'use_sim_time': use_sim_time
-        }]
-    )
+    # robot_state_publisher_cmd = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{
+    #         'robot_description': robot_description,
+    #         'use_sim_time': use_sim_time
+    #     }]
+    # )
 
     spawn_robot_cmd = Node(
         package='gazebo_ros',
@@ -140,6 +143,8 @@ def generate_launch_description():
             '-x', x_pose,
             '-y', y_pose,
             '-z', z_pose,
+            '-R', roll,
+            '-P', pitch,
             '-Y', yaw
         ],
         output='screen'
@@ -149,8 +154,10 @@ def generate_launch_description():
 
     ld.add_action(DeclareLaunchArgument('x_pose', default_value='1.5'))
     ld.add_action(DeclareLaunchArgument('y_pose', default_value='1.5'))
-    ld.add_action(DeclareLaunchArgument('z_pose', default_value='0.2'))
-    ld.add_action(DeclareLaunchArgument('yaw', default_value='4.712'))
+    ld.add_action(DeclareLaunchArgument('z_pose', default_value='1.38'))
+    ld.add_action(DeclareLaunchArgument('roll', default_value='0.0'))
+    ld.add_action(DeclareLaunchArgument('pitch', default_value='1.5708'))
+    ld.add_action(DeclareLaunchArgument('yaw', default_value='1.5708'))
     ld.add_action(DeclareLaunchArgument('use_sim_time', default_value='true'))
 
     ld.add_action(SetEnvironmentVariable('GAZEBO_MODEL_DATABASE_URI', ''))
@@ -158,7 +165,7 @@ def generate_launch_description():
 
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
+    # ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_robot_cmd)
 
     return ld
